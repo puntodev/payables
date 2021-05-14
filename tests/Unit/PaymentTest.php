c2be68d4f7af6d8891f21347b62877acaa45aabf
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-14 19:00:54 -0300

    wip

--- tests/Unit/PaymentTest.php
+++ tests/Unit/PaymentTest.php
@@ -4,6 +4,8 @@
 namespace Tests\Unit;
 
 
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Puntodev\Payables\Models\Payment;
 use Tests\TestCase;
@@ -13,7 +15,7 @@ class PaymentTest extends TestCase {
     use RefreshDatabase;
 
     /** @test */
-    function amount_has_is_factored_by_100()
+    function amount_is_factored_by_100()
     {
         $payment = Payment::factory()->create([
             'amount' => 14,
@@ -22,6 +24,38 @@ class PaymentTest extends TestCase {
         $this->assertDatabaseHas('payments', [
             'id' => $payment->id,
             'amount' => 1400,
+        ]);
+    }
+
+    /** @test */
+    function payable_relationship_is_morph() {
+        $payment = Payment::factory()->create();
+        $this->assertInstanceOf(MorphTo::class, $payment->payable());
+    }
+
+    /** @test */
+    function lookup_by_secondary_key() {
+        Payment::factory()->create([
+            'payment_method' => 'mercado_pago',
+            'payment_reference' => 'P123',
+        ]);
+
+        $this->assertDatabaseHas('payments', [
+            'payment_method' => 'mercado_pago',
+            'payment_reference' => 'P123',
+        ]);
+    }
+    /** @test */
+    function secondary_key_is_unique() {
+        $this->expectException( QueryException::class);
+
+        Payment::factory()->create([
+            'payment_method' => 'mercado_pago',
+            'payment_reference' => 'P123',
+        ]);
+        Payment::factory()->create([
+            'payment_method' => 'mercado_pago',
+            'payment_reference' => 'P123',
         ]);
     }
 }
