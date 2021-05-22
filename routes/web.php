de9e1894dbb9d47c464cfe0f49b737679322312a
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-22 13:03:11 -0300

    Only register routes for enabled gateways

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Puntodev\Payables\Http\Controllers\PaymentsWebhookController;
 
-Route::get('/payments/{gateway}', [PaymentsWebhookController::class, 'incoming'])
-    ->name('payment.incoming');
+Route::post('/{gateway}', [PaymentsWebhookController::class, 'incoming'])
+    ->name('payment.incoming')
+    ->where([
+        'gateway' => implode('|', array_keys(config('payments.gateways'))),
+    ]);
+
