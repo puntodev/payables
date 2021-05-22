ac582d6b319ab2ed189edd7034a207223d2f06e5
Mariano Goldman <mariano.goldman@puntodev.com.ar>
2021-05-22 10:51:54 -0300

    Return type  in facade

--- src/Facades/Payments.php
+++ src/Facades/Payments.php
@@ -3,12 +3,13 @@
 namespace Puntodev\Payables\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Puntodev\Payables\Contracts\GatewayPaymentOrder;
 use Puntodev\Payables\Contracts\Merchant;
 use Puntodev\Payables\Contracts\PaymentOrder;
 use Puntodev\Payables\Payments as PaymentsFacade;
 
 /**
- * @method static void checkout(string $gateway, PaymentOrder $paymentOrder, Merchant $merchant)
+ * @method static GatewayPaymentOrder checkout(string $gateway, PaymentOrder $paymentOrder, Merchant $merchant)
  *
  * @see \Puntodev\Payables\Payments
  */
